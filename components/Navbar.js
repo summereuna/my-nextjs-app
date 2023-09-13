14c85b054af67ec752b06d2f518476dfaa8911a0
summereuna <summereuna@gmail.com>
2023-09-13 18:22:39 +0900

    Add styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function NavBar() {
@@ -7,23 +8,43 @@ export default function NavBar() {
 
   return (
     <nav>
-      <Link className={router.pathname === "/" ? "active" : ""} href="/">
-        Home
-      </Link>
-      <Link
-        className={router.pathname === "/about" ? "active" : ""}
-        href="/about"
-      >
-        About
-      </Link>
+      <Image alt="logo" src="/vercel.svg" width={100} height={20} />
+      <div>
+        <Link className={router.pathname === "/" ? "active" : ""} href="/">
+          Home
+        </Link>
+        <Link
+          className={router.pathname === "/about" ? "active" : ""}
+          href="/about"
+        >
+          About
+        </Link>
+      </div>
       <style jsx global>{`
         nav {
-          background-color: tomato;
-          height: 2rem;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
         .active {
-          color: white;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <>
       <Seo title="About" />
+      <h1>About</h1>
     </>
   );
 }
